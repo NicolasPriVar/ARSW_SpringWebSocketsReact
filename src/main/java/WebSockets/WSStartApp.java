1413f87ead053a1ccd6dba01ad5bb409da310252
Nicolás Prieto Vargas <nicolas.prieto-v@labinfo.is.escuelaing.edu.co>
2025-07-03 12:12:17 -0500

    Creación del proyecto y funcionamiento de spring

---
+++ src/main/java/WebSockets/WSStartApp.java
@@ -0,0 +1,11 @@
+package WebSockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class WSStartApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(WSStartApp.class, args);
+    }
+}
